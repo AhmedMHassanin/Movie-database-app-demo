3864bef8f6d6f858959ea706352a5b24421753cd
AhmedMHassanin <ahmed.hassanin@web.de>
2023-02-27 18:12:55 +0100

    new change

--- movie.js
+++ movie.js
@@ -1,45 +1,41 @@
 async function getMovie(id) {
-  const movieReponse = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=e2c9f4dca9d4e03754e901bfc757e6a0`
-  );
+    const movieReponse = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=e2c9f4dca9d4e03754e901bfc757e6a0`
+    );
 
-  const data = await movieReponse.json();
-  return data;
+    const data = await movieReponse.json();
+    return data;
 }
 
-function changeTitle(title) {
-  const titleHeader = document.querySelector("#title");
-  titleHeader.innerHTML = title;
-}
 
 function changeTitle(title) {
-  const titleHeader = document.querySelector("#title");
-  titleHeader.innerHTML = title;
+    const titleHeader = document.querySelector("#title");
+    titleHeader.innerHTML = title;
 }
 
 function changeImg(imageSrc) {
-  const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-  const image = document.querySelector("#image");
-  image.src = `${IMG_PATH}/${imageSrc}`;
+    const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+    const image = document.querySelector("#image");
+    image.src = `${IMG_PATH}/${imageSrc}`;
 }
 
 function changeRate(rateText) {
-  const rate = document.querySelector("#rate");
-  rate.innerHTML = rateText;
+    const rate = document.querySelector("#rate");
+    rate.innerHTML = rateText;
 }
 
 function changeInfo(infoText) {
-  const info = document.querySelector("#info");
-  info.innerHTML = infoText;
+    const info = document.querySelector("#info");
+    info.innerHTML = infoText;
 }
 
 async function main() {
-  const id = localStorage.getItem("id");
-  const data = await getMovie(id);
-  changeTitle(data.original_title);
-  changeImg(data.poster_path);
-  changeRate(data.vote_average);
-  changeInfo(data.overview);
+    const id = localStorage.getItem("id");
+    const data = await getMovie(id);
+    changeTitle(data.original_title);
+    changeImg(data.poster_path);
+    changeRate(data.vote_average);
+    changeInfo(data.overview);
 }
 
 main();
